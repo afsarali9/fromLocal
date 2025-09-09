95af25109b16e66a3f9ac2633d7ad21067ae1979
Afsar Ali <afsarali9@gmail.com>
2025-09-10 03:37:15 +0530

    My Name component added and default images removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MyName from './components/MyName'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h3>Removed unneccesory code</h3>
+      <MyName />
     </>
   )
 }
---
+++ src/components/MyName.jsx
@@ -0,0 +1,7 @@
+const MyName = () => {
+  return (
+    <h3>This is GitHub</h3>
+  )
+}
+
+export default MyName
